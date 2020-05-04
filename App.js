6f3ab0cb50d68694cff19487c18be5372a2847f7
Alex Trew <trew13690@gmail.com>
2020-05-04 09:48:41 -0700

    touchable Component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
     const [people, setPeople] = useState([
@@ -12,6 +12,12 @@ export default function App() {
         {name: 'Jill', key: '7'}
     ]);
 
+    const pressHandler = (id) => {
+        console.log(id);
+        setPeople((prevPeople) => {
+            return prevPeople.filter(person => person.id != id)
+        })
+    }
   return (
   <View style={styles.container}>
    {/*   <ScrollView>
@@ -27,9 +33,10 @@ export default function App() {
          keyExtractor={(item)=> item.id}
          data={people}
          renderItem={({item}) => (
-             <Text styles={styles.item}>{item.name}</Text>
-         )}
-     />
+             <TouchableOpacity onPress={() => pressHandler(item.id)}>
+                 <Text style={styles.item}>{item.name}</Text>
+             </TouchableOpacity>
+             )}/>
   </View>
   );
 }
@@ -41,11 +48,11 @@ const styles = StyleSheet.create({
       paddingTop: 40,
       paddingHorizontal: 20
   },
-
     item:{
       marginTop: 24,
-        padding: 30,
-        backgroundColor: 'pink',
-        fontSize: 24
+      padding: 30,
+      margin: 10,
+      backgroundColor: 'pink',
+      fontSize: 24
     }
 });
